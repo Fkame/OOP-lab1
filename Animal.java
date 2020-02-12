eae2127fbdd245bda292baa1e2a41859142f2d2d
Fkame <nikitos_flame@mail.ru>
2020-02-12 21:30:59 +0300

    initial Animal class

---
+++ Animal.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class Animal {
+	
+	private String food = null;
+	private String hunger = null;
+	
+	public String makeNoice() {
+		return "*some animal voice*";
+	}
+	
+	public String eat() {
+		return "*some anial food*";
+	}
+	
+	public String sleep() {
+		return "*some animal sleep*";
+	}
+	
+	public String roam() {
+		return "*some animal roaming*";
+	}
+}
