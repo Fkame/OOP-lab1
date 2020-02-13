a9cb89f6531218c1dd0a419f87267c3a7754ace0
Fkame <nikitos_flame@mail.ru>
2020-02-13 12:27:53 +0300

    print dog and cat habits

--- AnimalHabits.java
+++ AnimalHabits.java
@@ -5,7 +5,7 @@ public class AnimalHabits {
 		System.out.println("Greeting in low-cost animal-habits text simulation prog!");
 		System.out.println("--------------------------------------------------------\n");
 		
-		//Вызов кошака
+		// Вызов кошака
 		Cat booris = new Cat();
 		System.out.println("Hi, i am Cat - Booris and now you are my own slave");
 		System.out.println(booris.makeNoice());
@@ -23,6 +23,24 @@ public class AnimalHabits {
 			System.out.println(str);
 		}
 		
+		// Вызов собаки
+		Dog fido = new Dog();
+		System.out.println("\n-----------------------------------------------------------");
+		System.out.println("Hi, i am son of proud Wolves - but i still love you, master");
+		System.out.println(fido.makeNoice());
+		System.out.println(fido.eat());
+		System.out.println(fido.roam());
+		System.out.println(fido.sleep());
+		System.out.println("");
+		
+		fido.setHunger(AnimalStatuses.HungerLevels.MID);
+		System.out.println(fido.eat());
+		
+		System.out.println("\nPicture of Dog:");
+		String[] picture_dog = fido.getPicture();
+		for (String str : picture_dog) {
+			System.out.println(str);
+		}
 	}
 	
 }
